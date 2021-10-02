da661ae21c62919fe141422f9f90b228389ba2d8
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-02 13:57:20 +0300

    Add comment to methods

--- core/Request.php
+++ core/Request.php
@@ -4,9 +4,11 @@ namespace Core;
 
 class Request
 {
-
-
-    // Get path from $_SERVER['REQUEST_URI'] global variable
+    /**
+     * Get path from $_SERVER['REQUEST_URI'] global variable
+     * 
+     * @return string $path
+     */
     public function getPath()
     {
         //Path is home if REQUEST_URI is null
@@ -22,7 +24,12 @@ class Request
         return substr($path, 0, $position);
     }
 
-    // Get path from $_SERVER['REQUEST_METHOD'] global variable
+    // 
+    /**
+     * Get path from $_SERVER['REQUEST_METHOD'] global variable
+     * 
+     * @return string $method
+     */
     public function getMethod()
     {
         $method = strtolower($_SERVER['REQUEST_METHOD']);
@@ -30,12 +37,23 @@ class Request
         return $method;
     }
 
+    /**
+     * Reads params from request
+     * 
+     * @return Array $data
+     */
     public function getBody()
     {
         $data = $_REQUEST;
         return $data;
     }
 
+    /**
+     * Return request parameter if someone try access undefined property
+     * 
+     * @param string $name
+     * @return Request $property 
+     */
     public function __get($name)
     {       
         $bodyData = $this->getBody();
