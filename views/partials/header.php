52ea550bee7c5498b367ff50203a292894e4ad1e
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-02 16:30:23 +0300

    Separate theme's parts

---
+++ views/partials/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="zxx">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>World Time</title>
+    <!-- plugin css for this page -->
+    <link
+      rel="stylesheet"
+      href="vendors/mdi/css/materialdesignicons.min.css"
+    />
+    <link rel="stylesheet" href="vendors/aos/dist/aos.css/aos.css" />
+
+    <!-- End plugin css for this page -->
+    <link rel="shortcut icon" href="images/favicon.png" />
+
+    <!-- inject:css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- endinject -->
+  </head>
+
+  <body>
+    <div class="container-scroller">
+      <div class="main-panel">
