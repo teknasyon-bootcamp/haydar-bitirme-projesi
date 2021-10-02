a31834c1f01af0555c67baa28f542b51459d335b
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-02 16:19:03 +0300

    Add theme assets

--- public/index.php
+++ public/index.php
@@ -2,14 +2,17 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use App\Controllers\TestController;
 use Core\Application;
 
 define('AppRootDirectory', dirname(__DIR__));
 
 $app = new Application();
 
-$app->router->get('/', 'home');
-$app->router->get('/contact','deneme.contact');
+$app->router->get('/','welcome');
+
+$app->router->post('/', [TestController::class,'index']);
+$app->router->get('/contact','contact');
 
 
 $app->run();
