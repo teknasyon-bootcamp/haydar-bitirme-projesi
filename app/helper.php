e55dc7b320942547058a12835907412dbedd937d
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-04 23:57:43 +0300

    Improve public_path function

--- app/helper.php
+++ app/helper.php
@@ -11,8 +11,7 @@ function view($viewAdress, ?array $data = [])
 
 function publicPath(?string $path = null)
 {
-    $dd = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . "/$path";
-    return $dd;
+    return $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . "/$path";
 }
 
 function dd(mixed $data)
