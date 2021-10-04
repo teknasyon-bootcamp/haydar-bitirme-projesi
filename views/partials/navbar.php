26dd2199839642f66b5ae02c815989fc77240c0e
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-04 23:42:16 +0300

    fix writing public path

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -36,7 +36,7 @@
       <div class="navbar-bottom">
         <div class="d-flex justify-content-between align-items-center">
           <div>
-            <a class="navbar-brand" href="#"><img src="<?php publicPath('images/logo.svg') ?>" alt="" /></a>
+            <a class="navbar-brand" href="#"><img src="<?= publicPath('images/logo.svg') ?>" alt="" /></a>
           </div>
           <div>
             <button class="navbar-toggler" type="button" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
