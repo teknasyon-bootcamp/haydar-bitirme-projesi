a9853166d9c12a0a4c9ed1ec3c93b019c1915f50
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-07 13:49:45 +0300

    Add postLogin route

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ $app->router->get('/contact','contact');
 $app->router->get('/register',[AuthController::class,'register'])->name('register');
 $app->router->post('/register',[UserController::class,'store']);
 $app->router->get('/login',[AuthController::class,'login'])->name('login');
+$app->router->post('/login',[UserController::class,'login'])->name('login');
