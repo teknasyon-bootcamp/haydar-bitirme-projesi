ce93b73d76aae2aa06a4a2509f89799594b333bf
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-07 23:17:40 +0300

    Add dashboard views

---
+++ views/manage/partials/footer.php
@@ -0,0 +1,15 @@
+    
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+
+    <!-- jQuery -->
+    <script src="<?= publicPath('manage/plugins/jquery/jquery.min.js') ?>"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?= publicPath('manage/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= publicPath('manage/dist/js/adminlte.min.js') ?>"></script>
+</body>
+
+</html>
---
+++ views/manage/partials/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Yönetim Paneli</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="<?= publicPath('manage/plugins/fontawesome-free/css/all.min.css') ?>">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= publicPath('manage/dist/css/adminlte.min.css') ?>">
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
