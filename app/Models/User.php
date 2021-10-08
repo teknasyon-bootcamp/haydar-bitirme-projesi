0a94878aefa2bd2242dc2eb5916046707eee1717
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-08 12:14:05 +0300

    Add getRole function

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Models;
 
@@ -6,4 +6,19 @@ use Core\Model;
 
 class User extends Model
 {
+    public function getRole()
+    {
+        $roleLevel = $this->role_level;
+        
+        if ($roleLevel == 1) {
+            return "Kullanıcı";
+        } elseif ($roleLevel == 2) {
+            return "Editör";
+        } elseif ($roleLevel == 3) {
+            return "Moderatör";
+        } elseif ($roleLevel == 4) {
+            return "Yönetici";
+        }
+
+    }
 }
