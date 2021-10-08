e80d17f66bac3724cc7777833b09b6e319345794
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-08 12:47:09 +0300

    Fix typo

--- views/manage/partials/sidebar.php
+++ views/manage/partials/sidebar.php
@@ -10,7 +10,7 @@
             <div class="sidebar">
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">İstek
+                    <div class="image">
                         <img src="<?= publicPath('manage/dist/img/user2-160x160.jpg') ?>" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
