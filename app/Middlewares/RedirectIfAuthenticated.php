f017dcdd7c7018c8c26fd93cf1926cd077b3645e
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-08 12:33:10 +0300

    Add type hinting to request variable

--- app/Middlewares/RedirectIfAuthenticated.php
+++ app/Middlewares/RedirectIfAuthenticated.php
@@ -3,16 +3,16 @@
 namespace App\Middlewares;
 
 use Core\Middleware\MiddlewareInterface;
+use Core\Request;
 
 class RedirectIfAuthenticated implements MiddlewareInterface
 {
-    public function handle($next, $request)
+    public function handle($next, Request $request)
     {
         if (!isGuest()) {
             redirect(route('manage.main'), true);
             exit;
         }
-
         return $next($request);
     }
 }
