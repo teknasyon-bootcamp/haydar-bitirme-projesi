d18337872d34fa16c73b446c5433962aa3615597
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-10 11:43:39 +0300

    Add File class

---
+++ core/File.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Core;
+
+class File
+{
+    public string $name;
+    public int $size;
+    public string $mimeType;
+    public string $tempPath;
+    public string $extension;
+    public string $OriginalName;
+
+    public function __construct(array $fileData)
+    {
+        $this->name = $fileData['name'];
+        $this->tempPath = $fileData['tmp_name'];
+        $this->extension =  pathinfo($this->name, PATHINFO_EXTENSION);
+        $this->originalName = basename($this->name, '.' . $this->extension);
+        $this->size = round(filesize($this->tempPath) / 1024, 1);
+
+        $fileInfo = finfo_open(FILEINFO_MIME_TYPE);
+        $this->mimeType = finfo_file($fileInfo, $this->tempPath);
+    }
+
+    public function move(string $path)
+    {
+        $uploadsFolder = AppRootDirectory . "/public/";
+
+        move_uploaded_file($this->tempPath, $uploadsFolder . $path);
+    }
+
+    public function isValidMimeType(array $allowedTypes)
+    {
+        $mimeTypeMatch = key_exists($this->mimeType, $allowedTypes);
+
+        $extensionMatch = false;
+
+        if ($mimeTypeMatch) {
+
+            if (is_string($allowedTypes[$this->mimeType])) {
+                $allowedTypes[$this->mimeType] = [$allowedTypes[$this->mimeType]];
+            }
+
+            $extensionMatch = in_array($this->extension, $allowedTypes[$this->mimeType]);
+        }
+
+
+        return $mimeTypeMatch === true && $extensionMatch === true;
+    }
+
+    public function getRandomizeName()
+    {
+        return $this->originalName . "-" . uniqid() . "." . $this->extension;
+    }
+}
