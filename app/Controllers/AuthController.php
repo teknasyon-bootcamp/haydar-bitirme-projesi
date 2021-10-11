4208d1d65ead12ab5594274271766233306690cc
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-11 10:55:41 +0300

    Send categories to views

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -2,20 +2,28 @@
 
 namespace App\Controllers;
 
+use App\Models\Category;
 use Core\Controller;
 use Core\Session\Session;
 
 class AuthController extends Controller
 {
+    public array $categories;
+
+    public function __construct()
+    {
+        $this->categories = Category::all();
+    }
+
     public function login()
     {
-        return view('login');
+        return view('login', ['categories' => $this->categories]);
     }
 
 
     public function register()
     {
-        return view('register');
+        return view('register', ['categories' => $this->categories]);
     }
 
     public function logout()
