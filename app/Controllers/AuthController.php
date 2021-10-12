a1fc403b886799934d84efe04ba549cc9d38e5ce
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-12 23:50:27 +0300

    Add logs

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Models\Category;
 use Core\Controller;
+use Core\Log\Logger;
 use Core\Session\Session;
 
 class AuthController extends Controller
@@ -17,17 +18,24 @@ class AuthController extends Controller
 
     public function login()
     {
+        $log = new Logger();
+        $log->info("Giriş sayfası ziyaret ediliyor.");
         return view('login', ['categories' => $this->categories]);
     }
 
 
     public function register()
     {
+        $log = new Logger();
+        $log->info("Kayıt sayfası ziyaret ediliyor.");
         return view('register', ['categories' => $this->categories]);
     }
 
     public function logout()
     {
+        $log = new Logger();
+        $log->info("Uygulamadan çıkış yapıldı.");
+
         Session::delete('user_id');
         return redirect('/');
     }
