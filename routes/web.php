07594c079864090fb3b8d9c8aa8813ed69d9f84e
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-12 13:40:29 +0300

    Give Auth middleware to all dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -25,32 +25,32 @@ $app->router->post('/login', [UserController::class, 'login'])->name('login');
 
 $app->router->get('/manage/main', [UserController::class, 'main'])->name('manage.main')->middleware(Auth::class);
 
-$app->router->get('/manage/categories', [CategoryController::class, 'index'])->name('manage.category.index');
-$app->router->post('/manage/category/editor/add', [CategoryController::class, 'editorAdd'])->name('manage.category.editor.add');
-$app->router->delete('/manage/category/editor/delete', [CategoryController::class, 'editorDelete'])->name('manage.category.editor.destory');
-$app->router->get('/manage/categories/create', [CategoryController::class, 'create'])->name('manage.category.create');
-$app->router->post('/manage/categories/create', [CategoryController::class, 'store'])->name('manage.category.store');
-$app->router->get('/manage/category/edit', [CategoryController::class, 'edit'])->name('manage.category.edit');
-$app->router->put('/manage/category/edit', [CategoryController::class, 'update'])->name('manage.category.update');
-$app->router->delete('/manage/category/delete', [CategoryController::class, 'destroy'])->name('manage.category.destroy');
+$app->router->get('/manage/categories', [CategoryController::class, 'index'])->name('manage.category.index')->middleware(Auth::class);
+$app->router->post('/manage/category/editor/add', [CategoryController::class, 'editorAdd'])->name('manage.category.editor.add')->middleware(Auth::class);
+$app->router->delete('/manage/category/editor/delete', [CategoryController::class, 'editorDelete'])->name('manage.category.editor.destory')->middleware(Auth::class);
+$app->router->get('/manage/categories/create', [CategoryController::class, 'create'])->name('manage.category.create')->middleware(Auth::class);
+$app->router->post('/manage/categories/create', [CategoryController::class, 'store'])->name('manage.category.store')->middleware(Auth::class);
+$app->router->get('/manage/category/edit', [CategoryController::class, 'edit'])->name('manage.category.edit')->middleware(Auth::class);
+$app->router->put('/manage/category/edit', [CategoryController::class, 'update'])->name('manage.category.update')->middleware(Auth::class);
+$app->router->delete('/manage/category/delete', [CategoryController::class, 'destroy'])->name('manage.category.destroy')->middleware(Auth::class);
 
-$app->router->get('/manage/news', [NewsController::class, 'index'])->name('manage.news.index');
-$app->router->get('/manage/news/create', [NewsController::class, 'create'])->name('manage.news.create');
-$app->router->post('/manage/news/create', [NewsController::class, 'store'])->name('manage.news.store');
-$app->router->get('/manage/news/edit', [NewsController::class, 'edit'])->name('manage.news.edit');
-$app->router->put('/manage/news/edit', [NewsController::class, 'update'])->name('manage.news.update');
-$app->router->delete('/manage/news/delete', [NewsController::class, 'destroy'])->name('manage.news.destroy');
+$app->router->get('/manage/news', [NewsController::class, 'index'])->name('manage.news.index')->middleware(Auth::class);
+$app->router->get('/manage/news/create', [NewsController::class, 'create'])->name('manage.news.create')->middleware(Auth::class);
+$app->router->post('/manage/news/create', [NewsController::class, 'store'])->name('manage.news.store')->middleware(Auth::class);
+$app->router->get('/manage/news/edit', [NewsController::class, 'edit'])->name('manage.news.edit')->middleware(Auth::class);
+$app->router->put('/manage/news/edit', [NewsController::class, 'update'])->name('manage.news.update')->middleware(Auth::class);
+$app->router->delete('/manage/news/delete', [NewsController::class, 'destroy'])->name('manage.news.destroy')->middleware(Auth::class);
 
-$app->router->get('/manage/comment', [CommentController::class, 'index'])->name('manage.comment.index');
-$app->router->get('/manage/comment/edit', [CommentController::class, 'edit'])->name('manage.comment.edit');
-$app->router->put('/manage/comment/edit', [CommentController::class, 'update'])->name('manage.comment.update');
-$app->router->delete('/manage/comment', [CommentController::class, 'destory'])->name('manage.comment.destroy');
+$app->router->get('/manage/comment', [CommentController::class, 'index'])->name('manage.comment.index')->middleware(Auth::class);
+$app->router->get('/manage/comment/edit', [CommentController::class, 'edit'])->name('manage.comment.edit')->middleware(Auth::class);
+$app->router->put('/manage/comment/edit', [CommentController::class, 'update'])->name('manage.comment.update')->middleware(Auth::class);
+$app->router->delete('/manage/comment', [CommentController::class, 'destory'])->name('manage.comment.destroy')->middleware(Auth::class);
 
-$app->router->get('/manage/account/deleteRequest', [UserController::class, 'requestView'])->name('manage.user.request.index');
-$app->router->post('/manage/account/deleteRequest', [UserController::class, 'request'])->name('manage.user.delete.request');
-$app->router->delete('/manage/account/deleteRequest', [UserController::class, 'destroy'])->name('manage.user.destroy');
-$app->router->get('/manage/accounts', [UserController::class, 'index'])->name('manage.user.index');
-$app->router->put('/manage/account/role/update', [UserController::class, 'roleUpdate'])->name('manage.user.role.update');
-$app->router->get('/manage/account/news/seen', [UserController::class, 'seenNews'])->name('manage.user.news.seen');
+$app->router->get('/manage/account/deleteRequest', [UserController::class, 'requestView'])->name('manage.user.request.index')->middleware(Auth::class);
+$app->router->post('/manage/account/deleteRequest', [UserController::class, 'request'])->name('manage.user.delete.request')->middleware(Auth::class);
+$app->router->delete('/manage/account/deleteRequest', [UserController::class, 'destroy'])->name('manage.user.destroy')->middleware(Auth::class);
+$app->router->get('/manage/accounts', [UserController::class, 'index'])->name('manage.user.index')->middleware(Auth::class);
+$app->router->put('/manage/account/role/update', [UserController::class, 'roleUpdate'])->name('manage.user.role.update')->middleware(Auth::class);
+$app->router->get('/manage/account/news/seen', [UserController::class, 'seenNews'])->name('manage.user.news.seen')->middleware(Auth::class);
 
-$app->router->put('/manage/account/follow/category', [UserController::class, 'requestFollow'])->name('manage.user.follow.category');
+$app->router->put('/manage/account/follow/category', [UserController::class, 'requestFollow'])->name('manage.user.follow.category')->middleware(Auth::class);
