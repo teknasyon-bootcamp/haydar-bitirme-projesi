a089927f2ef2dc4a58c6f43708bafdfc04c7d98b
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-13 16:37:57 +0300

    Remove uselless test images

--- public/index.php
+++ public/index.php
@@ -6,7 +6,9 @@ use Core\Application;
 
 define('AppRootDirectory', dirname(__DIR__));
 
-
+if (env('MAINTANCE_MODE') === true) {
+    # code...
+}
 
 $app = new Application();
 include_once AppRootDirectory."/routes/web.php";
