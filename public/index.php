a9bf3737bfe0091f0623d5cdb6cb2d881b97586e
Haydar ŞAHİN <haydarsahin34@gmail.com>
2021-10-13 16:28:26 +0300

    Remove test code

--- public/index.php
+++ public/index.php
@@ -2,10 +2,11 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-use App\Controllers\TestController;
 use Core\Application;
 
 define('AppRootDirectory', dirname(__DIR__));
+
+
 
 $app = new Application();
 include_once AppRootDirectory."/routes/web.php";
